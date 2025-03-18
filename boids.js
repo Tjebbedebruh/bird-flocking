f078f058ff2c56360ad945c03abc1ca29372d6a0
LanaIdema <147701599+LanaIdema@users.noreply.github.com>
2025-03-18 13:57:14 +0100

    same visual range for all birds

--- boids.js
+++ boids.js
@@ -16,7 +16,7 @@ let simulationRunning = false;
 // Simulation settings
 let numBoids = 1189; // Amount of Boids on the canvas
 let visualRangeBoid = 50; // Visual range of the boids
-let visualRangePredator = 100; // Visual range of the predators
+let visualRangePredator = 50; // Visual range of the predators
 let ambushRange = 60;
 let speedLimit = 12;  // Speed limit of the birds
 let minDistance = 5; // Minimum distance between boids
